6200a7d780bf29d7198fa64835fbf7afce26c95a
David Cruz <davidsaki@gmail.com>
2024-03-01 19:12:28 -0500

    new changes

--- Auction.Logics/PnrpManager/PeerRegistrationManager.cs
+++ Auction.Logics/PnrpManager/PeerRegistrationManager.cs
@@ -9,5 +9,21 @@ namespace Auction.Logics.PnrpManager
 {
     public class PeerRegistrationManager : IPeerRegistrationRepository
     {
+        private PeerNameRegistration _peerNameRegistration = null;
+        public bool IsPeerRegistered => _peerNameRegistration != null && _peerNameRegistration.IsRegistered();
+
+        public string PeerUri { get; }
+
+        public PeerName PeerName { get; }
+
+        public void StartPeerRegistration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopPeerRegistration()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
